0c0ec5204a941016cc9a16c4a32f2f4615fd467c
mayankmodi <mohitmv0673@gmail.com>
2025-01-24 01:47:50 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/Service/UrlShortnerService.java
@@ -0,0 +1,60 @@
+package com.example.demo.Service;
+
+import com.example.demo.model.UrlData;
+import io.micrometer.common.util.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
+
+@Service
+public class UrlShortnerService {
+
+    final Map<String, String> shortLongMap = new HashMap<>();
+    final Map<String, String> longShortMap = new HashMap<>();
+
+    final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    private final Random random= new Random();
+    private static final int shortUrlLength = 10;
+
+    @Value("${baseUrl}")
+    private String baseUrl;
+
+
+    public String shortenUrl(UrlData urlData) {
+        if(Objects.isNull(urlData) || StringUtils.isBlank(urlData.getUrl())){
+            throw new IllegalArgumentException("Invalid URL");
+        }
+        String longUrl = urlData.getUrl();
+        if(longShortMap.containsKey(longUrl)){
+            return baseUrl+longShortMap.get(longUrl);
+        }
+
+        String code;
+        do {
+            code = generateRandomString(shortUrlLength);
+        } while (shortLongMap.containsKey(code));
+
+        shortLongMap.put(code,longUrl);
+        longShortMap.put(longUrl,code);
+        return baseUrl+code;
+    }
+
+    private String generateRandomString(int shortUrlLength) {
+        StringBuilder urlCode = new StringBuilder(shortUrlLength);
+        for(int i = 0; i < shortUrlLength; i++) {
+            urlCode.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return urlCode.toString();
+    }
+
+    public  String getUrl(String shortUrl){
+        return shortLongMap.get(shortUrl);
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/controller/ShortenerController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.Service.UrlShortnerService;
+import com.example.demo.model.UrlData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/url")
+public class ShortenerController {
+
+    @Autowired
+    public UrlShortnerService urlShortnerService;
+
+    @PostMapping("/shortener")
+    public ResponseEntity<String> shortUrl(@RequestBody UrlData urlData){
+        String shortUrl = urlShortnerService.shortenUrl(urlData);
+        return ResponseEntity.ok(shortUrl);
+    }
+
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<String> getGivenUrl(@PathVariable String shortUrl){
+        String longUrl = urlShortnerService.getUrl(shortUrl);
+        if(longUrl != null){
+            return ResponseEntity.status(302).header("Location", longUrl).build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+}
